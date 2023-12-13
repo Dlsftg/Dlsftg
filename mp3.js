ff6b0e5191e61b12f9b17d5d8efb0b48842a4a8e
Dlsftg <88189929+Dlsftg@users.noreply.github.com>
2023-12-13 12:28:07 +0700

    Create mp3.js

---
+++ mp3.js
@@ -0,0 +1,6 @@
+document.write(`
+<audio id="hiddenAudio" autoplay>
+  <source src="https://lw-sycdn.kuwo.cn/0b316d144308efecfee3ec2d9d433aeb/65793f16/resource/30106/trackmedia/M5000003HgY22cQQV3.mp3" type="audio/mp3">
+  Your browser does not support the audio element.
+</audio>
+`);
